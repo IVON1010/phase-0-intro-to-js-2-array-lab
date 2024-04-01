458f2df46b0a68c0f62e4e4fb19a97bd6467e857
yvonnendungu <ndunguyvonne10@gmail.com>
2024-04-01 07:47:51 +0300

    add files

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+console.log(cats)
+console.log(cats);
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+destructivelyAppendCat("Ralph");
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+destructivelyPrependCat("Rosebud");
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+destructivelyRemoveLastCat("Aimy")
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift();
+}
+destructivelyRemoveFirstCat()
+
+function appendCat(name) {
+    return [...cats, name];
+}
+appendCat("Ruby")
+
+function prependCat(name) {
+    return [name,...cats];
+}
+prependCat("Gracie");
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
+function removeLastCat() {
+    return cats.slice(0, cats.length-1);
+}
